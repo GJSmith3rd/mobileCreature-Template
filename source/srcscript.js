63f3d2588e984caca4f918c2776a0878b2a35fc4
Gilbert Smith <gjsmith3rd@gmail.com>
2016-05-09 14:41:28 -0500

    Adding simple code to stopping ads in dev mode.

--- source/srcscript.js
+++ source/srcscript.js
@@ -2,4 +2,18 @@
 
 $(document).ready(function() {
 
+    var stopGoogleAds = false;
+
+    //Test for local dev network
+    if (/^10.0.0/.test(location.hostname) && stopGoogleAds) {
+
+        // Remove Adsense from DOM
+        $('.adsense').remove();
+
+    } else {
+
+        // Load Adsense JavaScript
+        $.getScript('//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js');
+    }
+
 });
