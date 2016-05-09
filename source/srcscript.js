6012fc67f039b04b0c66dae0ac67057b62bfdd3b
Gilbert Smith <gjsmith3rd@gmail.com>
2016-05-09 15:35:42 -0500

    Adding code for cloudinary projects.

--- source/srcscript.js
+++ source/srcscript.js
@@ -16,4 +16,21 @@ $(document).ready(function() {
         $.getScript('//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js');
     }
 
+      /*
+  * Cloudinary jQuery/JS Dynamic Images
+  */
+
+  (function () {
+
+    var bG = 'http://res.cloudinary.com/mobilecreature/image/upload/f_auto/v1456547839/FreeCodeCamp/Ziplines/Pomodoro-Timer/IeZm7MT.jpg';
+
+    var cBg = 'IeZm7MT.jpg';
+
+    $.cloudinary.config(
+      {
+        cloud_name: 'mobilecreature',
+        api_key: '222854649779236'
+      }
+    );
+
 });
